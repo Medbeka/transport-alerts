af5ce5b114c0873c849e3cab77f4e254b269ffdd
Medbeka <themehdi121@gmail.com>
2026-06-08 21:56:38 +0300

    Auto-run database migrations

--- Program.cs
+++ Program.cs
@@ -28,4 +28,11 @@ app.UseStaticFiles();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
+}
+
 app.Run();
